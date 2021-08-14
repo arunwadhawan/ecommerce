ffab08a254e51ebe9526a35b894238bd4dc7448d
arunwadhawan <wa.arun@gmail.com>
2021-08-14 19:55:15 +0530

    Update urls.py
    User verification logic

--- ecommerce/accounts/urls.py
+++ ecommerce/accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Leave as empty string for base url
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),    
+    path('logout/', views.user_logout, name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 
 ]
